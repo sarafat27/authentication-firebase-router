3157e7950bfe474d7f96ddd562e077b32c27c924
sarafat27 <sarafat2769@gmail.com>
2022-04-10 00:53:56 +0600

    sign out method implemented

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFireBase from '../Hooks/useFirebase';
 import './Header.css'
 const Header = () => {
+    const { user, handleSignOut } = useFireBase();
     return (
         <div className='header'>
             <nav>
                 <Link to='/'>Home</Link>
-                <Link to='/login'>Login</Link>
+                <Link to='/products'>products</Link>
+                <Link to='/orders'>Orders</Link>
+                <Link to='/register'>Register</Link>
+                <span>{user?.displayName && user.displayName}</span>
+                {user?.uid ?
+                    <button onClick={handleSignOut}>
+                        <Link to='/signOut'>sign out</Link>
+                    </button> :
+                    <Link to='/login'>Login</Link>
+                }
             </nav>
         </div>
     );
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFireBase from '../../Hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFireBase();
     return (
         <div>
-            <h2>This is login</h2>
+            <h2>Please login</h2>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google sign in</button>
+            </div>
+            <form>
+                <input type="email" name="" id="" placeholder='your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
